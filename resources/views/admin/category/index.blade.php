bab1071d4e128b517479d9e2e2ffaa7d67a98f44
Dana Sugu <dana.sugu@gmail.com>
2021-07-23 21:35:17 +0300

    Update index.blade.php

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -13,7 +13,7 @@
                 <div class="col-md-8">
 
                     @if(session('success'))
-                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
                         <strong>{{ session('success') }}!</strong>
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
