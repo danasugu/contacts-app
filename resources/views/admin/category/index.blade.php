d035781fe11a4de9f5c2cb84773ad1d6ccdb9a4e
Dana Sugu <dana.sugu@gmail.com>
2021-07-23 20:31:00 +0300

    Update index.blade.php

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -12,11 +12,12 @@
             <div class="row">
                 <div class="col-md-8">
 
-                    @if(session)
+                    @if(session('success'))
                     <div class="alert alert-warning alert-dismissible fade show" role="alert">
-  <strong>Holy guacamole!</strong>
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
+                        <strong>{{ session('success') }}!</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    @endif
                     <div class="card-header">
                         All categories
                     </div>
