c34783d69cdd81479d86ad8ea72308fb004bdae8
Dana Sugu <dana.sugu@gmail.com>
2021-07-24 07:50:38 +0300

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -31,12 +31,12 @@ class CategoryController extends Controller
             ]
         );
 
-        //var 1
-        // Category::insert([
-        //     'category_name' => $request->category_name,
-        //     'user_id' => Auth::user()->id,
-        //     'created_at' => Carbon::now()
-        // ]);
+        //var 1 Eloquet ORM
+        Category::insert([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+            'created_at' => Carbon::now()
+        ]);
 
         //var 2 Eloquent
 
@@ -48,10 +48,10 @@ class CategoryController extends Controller
 
 
             //var 3 Query Builder
-            $data = array();
-            $data['category_name'] = $request->category_name;
-            $data['user_id'] = Auth::user()->id;
-            DB::table('categories')->insert($data);
+            // $data = array();
+            // $data['category_name'] = $request->category_name;
+            // $data['user_id'] = Auth::user()->id;
+            // DB::table('categories')->insert($data);
 
             return Redirect()->back()->with('success', 'Category inserted succesfully');
     }
