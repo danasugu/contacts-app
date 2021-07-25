d3b6f4d22bd0621f995d52dbfdc77a57d98e3ce9
Dana Sugu <dana.sugu@gmail.com>
2021-07-25 07:58:43 +0300

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -58,40 +58,4 @@ class CategoryController extends Controller
 
             return Redirect()->back()->with('success', 'Category inserted succesfully');
     }
-
-     public function search(Request $request)
-    {
-        //get the general information about the website
-        // $website = Category::query()->firstOrFail();
-
-        $key = trim($request->get('q'));
-
-        $posts = Category::query()
-            ->where('category_name', 'like', "%{$key}%")
-            // ->orWhere('content', 'like', "%{$key}%")
-            // ->orderBy('created_at', 'desc')
-            ->get();
-
-        //get all the categories
-        $categories = Category::all();
-
-        //get all the tags
-        // $tags = Tag::all();
-
-        //get the recent 5 posts
-        $recent_posts = Category::query()
-            ->where('user_id', true)
-            ->orderBy('created_at', 'desc')
-            ->take(5)
-            ->get();
-
-        return view('search', [
-            // 'website' => $website,
-            'key' => $key,
-            // 'posts' => $posts,
-            'categories' => $categories,
-            // 'tags' => $tags,
-            // 'recent_posts' => $recent_posts
-        ]);
-    }
 }
