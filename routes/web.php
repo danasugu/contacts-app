d5f5571c8ae28a976c09e2d857122d15dd847738
Dana Sugu <dana.sugu@gmail.com>
2021-07-25 07:26:01 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,32 +30,3 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Route::get('/categories/all', [CategoryController::class, 'AllCat'])->name('all.category');
 Route::post('/category/add', [CategoryController::class, 'AddCat'])->name('store.category');
 
-// Route::post('/search', function() 
-//         {
-//             $q = request::get('q');
-//             // dd($q);
-//             if($q != "")
-//             {
-//                 $user = Category::where('category_name', 'LIKE', '%' . $q . '%')
-//                                     ->orWhere('user_id', 'LIKE', '%' . $q .'%')
-//                                     ->get();
-                    
-//                     // if(count($category) > 0)
-//                     return view('admin.category.index')->withDetails($categories)->withQuery($q);
-                                    
-//             } 
-            
-//             return "no results found!";
-
-//         });
-
-//         Route::any ( '/search', function () {
-//     $q = request::get ( 'q' );
-//     $user = User::where ( 'name', 'LIKE', '%' . $q . '%' )->orWhere ( 'email', 'LIKE', '%' . $q . '%' )->get ();
-//     if (count ( $user ) > 0)
-//         return view ( 'welcome' )->withDetails ( $user )->withQuery ( $q );
-//     else
-//         return view ( 'welcome' )->withMessage ( 'No Details found. Try to search again !' );
-// } );
-
-Route::get('/welcome', [CategoryController::class, 'search']);
